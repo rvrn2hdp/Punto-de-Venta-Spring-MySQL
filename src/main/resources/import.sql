79af9e5ff435f1d30e21ccec7f747661f9520e6e
JuanOrtiz <rvrn2hdp@gmail.com>
2021-10-19 00:23:23 -0300

    ultimos cambios, no funciona

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO `pdv_db`.`productos` (`act`, `cod_bar`, `descripcion`, `url_img`, `precio`, `stock`) 
+VALUES (1, '1111111111111', 'Arroz Carolina', 'https://thispersondoesnotexist.com/image', '10', '25');
